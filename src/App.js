d52c578448f6f553d19227d4786ec46453d91a82
vishaaal <officialvk02@gmail.com>
2021-11-27 19:11:53 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import { ThemeProvider } from '@mui/material/styles'
 import { CssBaseline } from '@mui/material'
 import theme from './theme'
-
+import './app.css'
+import bgvideo from './assets/Background video.mp4'
 import RouteComponent from './Routes'
 
 function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <RouteComponent />
-    </ThemeProvider>
+    <>
+      <video className="videoTag" autoPlay loop muted>
+        <source src={bgvideo} type="video/mp4" />
+      </video>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <RouteComponent />
+      </ThemeProvider>
+    </>
   )
 }
 
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import { useStyles } from './home.style'
 
+
 import { HomeLogic } from './home'
 
 function Home() {
   const { foo } = HomeLogic()
   console.log(foo)
   const classes = useStyles()
-  return <div className={classes.root}>Home Page</div>
+  return <div className={classes.root}></div>
 }
 
 export default Home
