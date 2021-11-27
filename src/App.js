863a2944c3a7e70e6daab551f7c3a378f005618f
Shubhamdutta2000 <dondutta2000@gmail.com>
2021-11-27 19:47:03 +0530

    refractore: structure home codebase

--- src/App.js
+++ src/App.js
@@ -2,15 +2,11 @@ import { ThemeProvider } from '@mui/material/styles'
 import { CssBaseline } from '@mui/material'
 import theme from './theme'
 import './app.css'
-import bgvideo from './assets/Background video.mp4'
 import RouteComponent from './Routes'
 
 function App() {
   return (
     <>
-      <video className="videoTag" autoPlay loop muted>
-        <source src={bgvideo} type="video/mp4" />
-      </video>
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <RouteComponent />
--- src/pages/Home/home.style.js
+++ src/pages/Home/home.style.js
@@ -2,4 +2,7 @@ import { makeStyles } from '@mui/styles'
 
 export const useStyles = makeStyles((theme) => ({
   root: {},
+  voot: {
+    color: "black"
+  }
 }))
