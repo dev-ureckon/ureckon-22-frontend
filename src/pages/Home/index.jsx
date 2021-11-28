171cc87de95a834d9cf6e7467ec58e820b4d6dd7
vishaaal <officialvk02@gmail.com>
2021-11-28 10:53:52 +0530

    Add: icon links

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useStyles } from './home.style'
 import { Paper } from '@mui/material'
 import Grid from '@mui/material/Grid'
@@ -14,14 +14,22 @@ import linkedinwhite from '../../assets/SVGs/linkedin logo white.svg'
 import twitterwhite from '../../assets/SVGs/twitter logo white.svg'
 import youtubewhite from '../../assets/SVGs/youtube logo white.svg'
 
+
+
 function Home() {
   const classes = useStyles()
+  // const[imgSrc, setimgSrc]=useState('')
+  // const handleonmouseOver =(e)=>{
+
+  }
   return (
+
+
     <div className={classes.root}>
       <video className="videoTag" autoPlay loop muted>
         <source src={bgvideo} type="video/mp4" />
       </video>
-
+      {/* 
       <Grid container align="center" sx={{ paddingTop: 2 }}>
         <Grid item lg={4}>
           <img src={uemLogo} alt="UEM" />
@@ -34,8 +42,8 @@ function Home() {
         </Grid>
       </Grid>
       <Grid container>
-        <Grid>
-          <Grid container lg={5} sx={{ paddingTop: 2 }}>
+        <Grid item>
+          <Grid container justifyContent="space-around" alignItems="center">
             <Grid item>
               <div>Login</div>
             </Grid>
@@ -43,7 +51,7 @@ function Home() {
               <div>More</div>
             </Grid>
           </Grid>
-          <Grid container lg={5} sx={{ paddingTop: 2 }}>
+          <Grid container justifyContent="space-around" alignItems="center">
             <Grid item>
               <div>Events</div>
             </Grid>
@@ -52,7 +60,7 @@ function Home() {
             </Grid>
           </Grid>
         </Grid>
-        <Grid>
+        <Grid item>
           <Grid item>
             <div>
               <img src={fbwhite} alt="heading" />
@@ -82,6 +90,88 @@ function Home() {
         </Grid>
         <Grid item lg={4}>
           <img src={asus} alt="UEM" />
+        </Grid>
+      </Grid> */}
+
+      {/* for main container */}
+      <Grid container>
+        {/* for Navbar */}
+        <Grid container align="center" sx={{ paddingTop: 5 }}>
+          <Grid item lg={4}>
+            <img src={uemLogo} alt="UEM" />
+          </Grid>
+          <Grid item lg={4}>
+            <img src={headingLogo} alt="UEM" />
+          </Grid>
+          <Grid item lg={4}>
+            <img src={ureckonLogo} alt="heading" />
+          </Grid>
+        </Grid>
+
+        {/* for Menu */}
+        <Grid container columns={13}>
+          <Grid item lg={12}>
+            <Grid container alignItems="center">
+              <Grid item lg={6}>
+                Login
+              </Grid>
+              <Grid item lg={6}>
+                More
+              </Grid>
+              <Grid item lg={6}>
+                Events
+              </Grid>
+              <Grid item lg={6}>
+                About
+              </Grid>
+            </Grid>
+          </Grid>
+          <Grid item>
+            <div>
+              <a
+                href="https://m.facebook.com/ureckon.uemk/?refid=52&__tn__=C-R"
+                target="_blank"
+              >
+                <img src={fbwhite} alt="heading" />
+              </a>
+            </div>
+            <div>
+              <a href="https://www.instagram.com/ureckon.uemk/" target="_blank">
+                <img src={instawhite} alt="heading" />
+              </a>
+            </div>
+            <div>
+              <a href="https://www.linkedin.com/company/ureckon/" target="_blank">
+                <img src={linkedinwhite} alt="heading" />
+              </a>
+            </div>
+            <div>
+              <a href="https://twitter.com/Ureckon_Uemk" target="_blank">
+                <img src={twitterwhite} alt="heading" />
+              </a>
+            </div>
+            <div>
+              <a href="https://www.youtube.com/c/UreckonUEMKolkata" target="_blank">
+                <img src={youtubewhite} alt="heading" />
+              </a>
+            </div>
+          </Grid>
+        </Grid>
+        <Grid
+          container
+          direction="row"
+          justifyContent="space-around"
+          alignItems="flex-end"
+        >
+          <Grid item>
+            <div className={classes.sponsored}>Sponsored By</div>
+          </Grid>
+          <Grid item>
+            <img src={repubgamers} alt="UEM" />
+          </Grid>
+          <Grid item>
+            <img src={asus} alt="UEM" />
+          </Grid>
         </Grid>
       </Grid>
     </div>
