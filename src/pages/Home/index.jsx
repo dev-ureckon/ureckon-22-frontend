23653fa6904f4a0d1a3bca70e67a39e70f4cf7c3
vishaaal <vishaaal@users.noreply.github.com>
2021-12-04 05:40:55 +0000

    (CI) Apply formatting changes

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -11,8 +11,8 @@ import './socialicons.css'
 import line1 from '../../assets/SVGs/Union.svg'
 import line2 from '../../assets/SVGs/unionTwo.svg'
 import { Link } from 'react-router-dom'
-import {moreFunction} from './home'
-import {backoptions} from './home'
+import { moreFunction } from './home'
+import { backoptions } from './home'
 
 function Home() {
   const classes = useStyles()
@@ -24,219 +24,216 @@ function Home() {
 
       {/* for main container */}
       {/* for Navbar */}
-      <Grid sx={{ paddingY:5}}>
-      <Grid
-        container
-        direction="row"
-        justifyContent="space-around"
-        className="headcontainer"
-        flexWrap="nowrap"
-        sx={{ paddingX: 30}}
-        xs={12}
-      >
-        <Grid item alignSelf="center" className="headimgcontain">
-          <img src={uemLogo} className="headimage1" alt="UEM" />
-        </Grid>
-        <Grid item className="headimgcontain2">
-          <img src={headingLogo} className="headimage2" alt="ureckon" />
-        </Grid>
-        <Grid item className="headimgcontain">
-          <img src={ureckonLogo} className="headimage3" alt="ureckonlogo" />
-        </Grid>
-      </Grid>
-      <Grid container>
-        {/* for Menu */}
-        <Grid
-          container
-          columns={{ lg: 13, sm: 12, xs: 12 }}
-          className="menuwithsocial"
-          justifyContent={{ md: 'center', xs: 'center', sm: 'center' }}
-        >
-          <Grid item lg={12} className="menupad" id="mainpage">
-            <Grid className="menufont" container alignItems="center">
-              <Grid item container justifyContent="center" lg={6} xs={12}>
-                <Link to="" className="menulink">
-                  Login
-                  <div>
-                    <img className="test" src={line1} alt="line" />
-                  </div>
-                </Link>
-              </Grid>
-              <Grid
-                item
-                container
-                justifyContent={{ lg: 'flex-end', xs: 'center' }}
-                lg={6}
-                xs={12}
-                order={{ lg: 2, xs: 4 }}
-              >
-                <div onClick={moreFunction}>
-                <Link to="" className="menulink">
-                  More..
-                  <div>
-                    <img className="test2" src={line2} alt="line" />
-                  </div>
-                </Link>
-                
-                </div>
-              </Grid>
-              <Grid
-                item
-                container
-                justifyContent={{ lg: 'flex-end', xs: 'center' }}
-                lg={6}
-                xs={12}
-                order={{ lg: 3, xs: 2 }}
-              >
-                <Link to="" className="menulink">
-                  Events
-                  <div>
-                    <img className="test3" src={line1} alt="line" />
-                  </div>
-                </Link>
-              </Grid>
-              <Grid
-                item
-                container
-                justifyContent="center"
-                lg={6}
-                xs={12}
-                order={{ lg: 4, xs: 3 }}
-              >
-                <Link to="" className="menulink">
-                  About
-                  <div>
-                    <img className="test4" src={line2} alt="line" />
-                  </div>
-                </Link>
-              </Grid>
-            </Grid>
-          </Grid>
-
-          {/* -------------------------------------------------------------------------------- */}
-
-          <Grid item lg={12} className="menupad2" id="moreoptions">
-            <Grid className="menufont" container alignItems="center">
-              <Grid item container justifyContent="center" lg={6} xs={12}>
-              
-                <Link to="" className="menulink">
-                  Team
-                  <div>
-                    <img className="test" src={line1} alt="line" />
-                  </div>
-                </Link>
-                
-              </Grid>
-              <Grid
-                item
-                container
-                justifyContent={{ lg: 'flex-end', xs: 'center' }}
-                lg={6}
-                xs={12}
-                order={{ lg: 2, xs: 4 }}
-              >
-                <div onClick={backoptions}>
-                <Link to="" className="menulink">
-                  Back..
-                  <div>
-                    <img className="test2" src={line2} alt="line" />
-                  </div>
-                </Link>
-                </div>
-              </Grid>
-              <Grid
-                item
-                container
-                justifyContent={{ lg: 'flex-end', xs: 'center' }}
-                lg={6}
-                xs={12}
-                order={{ lg: 3, xs: 2 }}
-              >
-                <Link to="" className="menulink">
-                  Contact
-                  <div>
-                    <img className="test5" src={line1} alt="line" />
-                  </div>
-                </Link>
-              </Grid>
-              <Grid
-                item
-                container
-                justifyContent="center"
-                lg={6}
-                xs={12}
-                order={{ lg: 4, xs: 3 }}
-              >
-                <Link to="" className="menulink">
-                  Sponsors
-                  <div>
-                    <img className="test6" src={line2} alt="line" />
-                  </div>
-                </Link>
-              </Grid>
-            </Grid>
-          </Grid>
-
-          {/* ----------------------------------------------------------------------------------------- */}
-
-          <Grid
-            item
-            display="flex"
-            flexDirection={{ lg: 'column', md: 'row', xs: 'row' }}
-            alignItems={{ xs: 'center', md: 'center' }}
-            justifyContent={{ md: 'center', xs: 'center', sm: 'center' }}
-            className="socialicons"
-            xs={12}
-            lg={1}
-          >
-            <Grid item>
-              <a
-                href="https://m.facebook.com/ureckon.uemk/?refid=52&__tn__=C-R"
-                target="_blank"
-              >
-                <div className="facebook"></div>
-              </a>
-            </Grid>
-            <Grid item sx={{ my: 1 }}>
-              <a href="https://www.instagram.com/ureckon.uemk/" target="_blank">
-                <div className="instagram"></div>
-              </a>
-            </Grid>
-            <Grid item>
-              <a href="https://www.linkedin.com/company/ureckon/" target="_blank">
-                <div className="linkedin"></div>
-              </a>
-            </Grid>
-            <Grid item sx={{ my: 1 }}>
-              <a href="https://twitter.com/Ureckon_Uemk" target="_blank">
-                <div className="twitter"></div>
-              </a>
-            </Grid>
-            <Grid item>
-              <a href="https://www.youtube.com/c/UreckonUEMKolkata" target="_blank">
-                <div className="youtube"></div>
-              </a>
-            </Grid>
-          </Grid>
-        </Grid>
+      <Grid sx={{ paddingY: 5 }}>
         <Grid
           container
           direction="row"
           justifyContent="space-around"
-          sx={{ paddingX: 31 }}
+          className="headcontainer"
           flexWrap="nowrap"
+          sx={{ paddingX: 30 }}
+          xs={12}
         >
-          <Grid item alignSelf="center">
-            <div className="Sponsor">Sponsored By</div>
+          <Grid item alignSelf="center" className="headimgcontain">
+            <img src={uemLogo} className="headimage1" alt="UEM" />
+          </Grid>
+          <Grid item className="headimgcontain2">
+            <img src={headingLogo} className="headimage2" alt="ureckon" />
           </Grid>
           <Grid item className="headimgcontain">
-            <img src={repubgamers} className="headimage1" alt="UEM" />
-          </Grid>
-          <Grid item sx={{ paddingTop: 3 }} className="headimgcontain">
-            <img src={asus} className="headimage1" alt="UEM" />
+            <img src={ureckonLogo} className="headimage3" alt="ureckonlogo" />
           </Grid>
         </Grid>
-      </Grid>
+        <Grid container>
+          {/* for Menu */}
+          <Grid
+            container
+            columns={{ lg: 13, sm: 12, xs: 12 }}
+            className="menuwithsocial"
+            justifyContent={{ md: 'center', xs: 'center', sm: 'center' }}
+          >
+            <Grid item lg={12} className="menupad" id="mainpage">
+              <Grid className="menufont" container alignItems="center">
+                <Grid item container justifyContent="center" lg={6} xs={12}>
+                  <Link to="" className="menulink">
+                    Login
+                    <div>
+                      <img className="test" src={line1} alt="line" />
+                    </div>
+                  </Link>
+                </Grid>
+                <Grid
+                  item
+                  container
+                  justifyContent={{ lg: 'flex-end', xs: 'center' }}
+                  lg={6}
+                  xs={12}
+                  order={{ lg: 2, xs: 4 }}
+                >
+                  <div onClick={moreFunction}>
+                    <Link to="" className="menulink">
+                      More..
+                      <div>
+                        <img className="test2" src={line2} alt="line" />
+                      </div>
+                    </Link>
+                  </div>
+                </Grid>
+                <Grid
+                  item
+                  container
+                  justifyContent={{ lg: 'flex-end', xs: 'center' }}
+                  lg={6}
+                  xs={12}
+                  order={{ lg: 3, xs: 2 }}
+                >
+                  <Link to="" className="menulink">
+                    Events
+                    <div>
+                      <img className="test3" src={line1} alt="line" />
+                    </div>
+                  </Link>
+                </Grid>
+                <Grid
+                  item
+                  container
+                  justifyContent="center"
+                  lg={6}
+                  xs={12}
+                  order={{ lg: 4, xs: 3 }}
+                >
+                  <Link to="" className="menulink">
+                    About
+                    <div>
+                      <img className="test4" src={line2} alt="line" />
+                    </div>
+                  </Link>
+                </Grid>
+              </Grid>
+            </Grid>
+
+            {/* -------------------------------------------------------------------------------- */}
+
+            <Grid item lg={12} className="menupad2" id="moreoptions">
+              <Grid className="menufont" container alignItems="center">
+                <Grid item container justifyContent="center" lg={6} xs={12}>
+                  <Link to="" className="menulink">
+                    Team
+                    <div>
+                      <img className="test" src={line1} alt="line" />
+                    </div>
+                  </Link>
+                </Grid>
+                <Grid
+                  item
+                  container
+                  justifyContent={{ lg: 'flex-end', xs: 'center' }}
+                  lg={6}
+                  xs={12}
+                  order={{ lg: 2, xs: 4 }}
+                >
+                  <div onClick={backoptions}>
+                    <Link to="" className="menulink">
+                      Back..
+                      <div>
+                        <img className="test2" src={line2} alt="line" />
+                      </div>
+                    </Link>
+                  </div>
+                </Grid>
+                <Grid
+                  item
+                  container
+                  justifyContent={{ lg: 'flex-end', xs: 'center' }}
+                  lg={6}
+                  xs={12}
+                  order={{ lg: 3, xs: 2 }}
+                >
+                  <Link to="" className="menulink">
+                    Contact
+                    <div>
+                      <img className="test5" src={line1} alt="line" />
+                    </div>
+                  </Link>
+                </Grid>
+                <Grid
+                  item
+                  container
+                  justifyContent="center"
+                  lg={6}
+                  xs={12}
+                  order={{ lg: 4, xs: 3 }}
+                >
+                  <Link to="" className="menulink">
+                    Sponsors
+                    <div>
+                      <img className="test6" src={line2} alt="line" />
+                    </div>
+                  </Link>
+                </Grid>
+              </Grid>
+            </Grid>
+
+            {/* ----------------------------------------------------------------------------------------- */}
+
+            <Grid
+              item
+              display="flex"
+              flexDirection={{ lg: 'column', md: 'row', xs: 'row' }}
+              alignItems={{ xs: 'center', md: 'center' }}
+              justifyContent={{ md: 'center', xs: 'center', sm: 'center' }}
+              className="socialicons"
+              xs={12}
+              lg={1}
+            >
+              <Grid item>
+                <a
+                  href="https://m.facebook.com/ureckon.uemk/?refid=52&__tn__=C-R"
+                  target="_blank"
+                >
+                  <div className="facebook"></div>
+                </a>
+              </Grid>
+              <Grid item sx={{ my: 1 }}>
+                <a href="https://www.instagram.com/ureckon.uemk/" target="_blank">
+                  <div className="instagram"></div>
+                </a>
+              </Grid>
+              <Grid item>
+                <a href="https://www.linkedin.com/company/ureckon/" target="_blank">
+                  <div className="linkedin"></div>
+                </a>
+              </Grid>
+              <Grid item sx={{ my: 1 }}>
+                <a href="https://twitter.com/Ureckon_Uemk" target="_blank">
+                  <div className="twitter"></div>
+                </a>
+              </Grid>
+              <Grid item>
+                <a href="https://www.youtube.com/c/UreckonUEMKolkata" target="_blank">
+                  <div className="youtube"></div>
+                </a>
+              </Grid>
+            </Grid>
+          </Grid>
+          <Grid
+            container
+            direction="row"
+            justifyContent="space-around"
+            sx={{ paddingX: 31 }}
+            flexWrap="nowrap"
+          >
+            <Grid item alignSelf="center">
+              <div className="Sponsor">Sponsored By</div>
+            </Grid>
+            <Grid item className="headimgcontain">
+              <img src={repubgamers} className="headimage1" alt="UEM" />
+            </Grid>
+            <Grid item sx={{ paddingTop: 3 }} className="headimgcontain">
+              <img src={asus} className="headimage1" alt="UEM" />
+            </Grid>
+          </Grid>
+        </Grid>
       </Grid>
     </div>
   )
