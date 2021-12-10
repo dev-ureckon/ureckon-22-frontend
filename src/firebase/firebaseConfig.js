62de29981b04f895805ceabc0f5e3da421d540e5
Prasundas99 <prasundas115@gmail.com>
2021-12-11 01:18:30 +0530

    fix: sdk setup keys

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -1,12 +1,12 @@
 // config file
 /*eslint-disable */
 export default {
-    apiKey: 'AIzaSyAYssrSI634VtrH4HxWwl5uxXWVTVp3qjE',
-    authDomain: 'iem-fbe93.firebaseapp.com',
-    projectId: 'iem-fbe93',
-    storageBucket: 'iem-fbe93.appspot.com',
-    messagingSenderId: '230632233744',
-    appId: '1:230632233744:web:f27fb35e5514163e5b2395',
-    measurementId: 'G-XZD3N23JTK'
+    apiKey: "AIzaSyB6DBydJtUd0G6lz_YiVjneaLsZ0bDrA_E",
+    authDomain: "ureckon22.firebaseapp.com",
+    projectId: "ureckon22",
+    storageBucket: "ureckon22.appspot.com",
+    messagingSenderId: "565199633168",
+    appId: "1:565199633168:web:e1e8dfe3effef57acc4395",
+    measurementId: "${config.measurementId}"
   }
   
