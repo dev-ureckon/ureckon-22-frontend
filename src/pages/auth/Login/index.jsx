499af3aa9084e71fec304a096a72239277a519ba
Shubhamdutta2000 <dondutta2000@gmail.com>
2021-12-12 20:26:23 +0530

    [FIX] refractore login page

--- src/pages/auth/Login/index.jsx
+++ src/pages/auth/Login/index.jsx
@@ -1,44 +1,14 @@
 import { useState, useEffect } from 'react'
-import { Link, useNavigate } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import { Alert, Button, Container, Grid, LinearProgress, Typography } from '@mui/material'
 import { useStyles } from '../auth.style.js'
 import Input from '../../../components/input'
 import SocialAuth from '../../../components/SocialAuth/index.jsx'
+import { LoginLogic } from './login.js'
 
-//Redux
-import { useDispatch, useSelector } from 'react-redux'
-import { userLogin } from '../../../redux/actions'
-
-const Login = (props) => {
-  const isMobile = window.innerWidth <= 768
-
+const Login = () => {
   const classes = useStyles()
-  const dispatch = useDispatch()
-  const navigate = useNavigate()
-
-  //getting state from reducer
-  const { userInfo, error, loading } = useSelector((state) => state.userLogin)
-
-  const [formData, setFormData] = useState({
-    email: '',
-    password: '',
-  })
-
-  const handleChange = (e) => {
-    setFormData((f) => ({
-      ...f,
-      [e.target.name]: e.target.value,
-    }))
-  }
-
-  //If user is already logged in Do not show this page
-  useEffect(() => {
-    if (userInfo) navigate('/')
-  }, [userInfo, navigate, loading])
-
-  const handleSubmit = (e) => {
-    dispatch(userLogin(formData.email, formData.password))
-  }
+  const { isMobile, dispatch, formData, setFormData, handleChange, handleSubmit, loading, error } = LoginLogic();
 
   return (
     <div className={classes.root}>
---
+++ src/pages/auth/Login/login.js
@@ -0,0 +1,38 @@
+import { useState, useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
+//Redux
+import { useDispatch, useSelector } from 'react-redux'
+import { userLogin } from '../../../redux/actions'
+
+
+export const LoginLogic = () => {
+    const isMobile = window.innerWidth <= 768
+    const dispatch = useDispatch()
+    const navigate = useNavigate()
+
+    //getting state from reducer
+    const { userInfo, error, loading } = useSelector((state) => state.userLogin)
+
+    const [formData, setFormData] = useState({
+        email: '',
+        password: '',
+    })
+
+    const handleChange = (e) => {
+        setFormData((f) => ({
+            ...f,
+            [e.target.name]: e.target.value,
+        }))
+    }
+
+    //If user is already logged in Do not show this page
+    useEffect(() => {
+        if (userInfo) navigate('/')
+    }, [userInfo, navigate, loading])
+
+    const handleSubmit = (e) => {
+        dispatch(userLogin(formData.email, formData.password))
+    }
+
+    return { isMobile, dispatch, formData, setFormData, handleChange, handleSubmit, loading, error }
+}
