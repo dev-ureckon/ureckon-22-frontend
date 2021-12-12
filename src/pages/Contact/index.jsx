872cd15fbfa9b23bede621ee85b85311a456fc62
souptik4572 <souptiksarkar4572@gmail.com>
2021-12-12 14:01:29 +0530

    added controller for creating new contact

--- src/pages/Contact/index.jsx
+++ src/pages/Contact/index.jsx
@@ -1,11 +1,42 @@
 import React from 'react'
 import { useStyles } from './contact.style'
-
-import { ContactLogic } from './contact'
+import { useInputState } from '../../hooks/useInputState'
+import { ContactLogic, createNewContact } from './contact'
 
 function Contact() {
-  const { foo } = ContactLogic()
-  console.log(foo)
+  // State for storing contact form data
+  // TODO: Inserting the below state and handleChange values for the different form inputs
+  const [name, handleNameChange, resetName] = useInputState('ABC XYZ')
+  const [email, handleEmailChange, resetEmail] = useInputState('abc@gmail.com')
+  const [subject, handleSubjectChange, resetSubject] = useInputState(
+    'This is a sample contact'
+  )
+  const [message, handleMessageChange, resetMessage] = useInputState(
+    'This is a sample message'
+  )
+
+  // Contact form submit handler
+  // TODO: Inserting the below handler after completion of UI
+  const handleFormSubmit = async (event) => {
+    event.preventDefault()
+    if (name && email && subject && message) {
+      const { success, msg } = await createNewContact({
+        name,
+        email,
+        subject,
+        message,
+      })
+      if (success) {
+        resetName()
+        resetEmail()
+        resetSubject()
+        resetMessage()
+      }
+    }
+  }
+
+  // const { foo } = ContactLogic()
+  // console.log(foo)
   const classes = useStyles()
 
   return <div className={classes.root}>Contact Page</div>
