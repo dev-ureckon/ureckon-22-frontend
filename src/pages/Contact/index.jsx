6df9d9b2458a99806c28fdf510e32abbdb56456c
Bihan001 <bhchakraborty001@gmail.com>
2021-12-16 15:34:16 +0530

    added: Contact Page UI

--- src/pages/Contact/index.jsx
+++ src/pages/Contact/index.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 import { useStyles } from './contact.style'
 import { useInputState } from '../../hooks/useInputState'
 import { ContactLogic, createNewContact } from './contact'
+import { Container, Grid } from '@mui/material'
+import Input from '../../components/input'
+import Button from '../../components/button'
 
 function Contact() {
   // State for storing contact form data
@@ -39,7 +42,59 @@ function Contact() {
   console.log(foo)
   const classes = useStyles()
 
-  return <div className={classes.root}>Contact Page</div>
+  return (
+    <Container>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault()
+          handleFormSubmit(e)
+        }}
+      >
+        <Grid container spacing={10}>
+          <Grid item lg={6} md={12} sm={12}>
+            <Input
+              width="100%"
+              label="Name"
+              placeholder="Your Name"
+              value={name}
+              onChange={(e) => handleNameChange(e)}
+            />
+          </Grid>
+          <Grid item lg={6} md={12} sm={12}>
+            <Input
+              width="100%"
+              label="Email"
+              placeholder="abc@gmail.com"
+              value={email}
+              onChange={(e) => handleEmailChange(e)}
+            />
+          </Grid>
+          <Grid item lg={6} md={12} sm={12}>
+            <Input
+              width="100%"
+              label="Subject"
+              placeholder="What's your question?"
+              value={subject}
+              onChange={(e) => handleSubjectChange(e)}
+            />
+          </Grid>
+          <Grid item lg={6} md={12} sm={12}>
+            <Input
+              width="100%"
+              label="Message"
+              placeholder="Your message"
+              value={message}
+              onChange={(e) => handleMessageChange(e)}
+              size="big"
+            />
+          </Grid>
+        </Grid>
+        <div style={{ display: 'flex', justifyContent: 'center' }}>
+          <Button>Submit</Button>
+        </div>
+      </form>
+    </Container>
+  )
 }
 
 export default Contact
