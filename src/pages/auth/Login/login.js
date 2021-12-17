02eabddfb6d0ae39563c73e7b0b1394874b693fe
Prasundas99 <prasundas115@gmail.com>
2021-12-17 20:37:51 +0530

    jhamela

--- src/pages/auth/Login/login.js
+++ src/pages/auth/Login/login.js
@@ -1,63 +1,37 @@
 import { useState, useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
 //Redux
 import { useDispatch, useSelector } from 'react-redux'
-import { useNavigate } from 'react-router-dom'
-import swal from 'sweetalert'
 import { userLogin } from '../../../redux/actions'
 
 export const LoginLogic = () => {
-  const isMobile = window.innerWidth <= 768
-  const dispatch = useDispatch()
-  const navigate = useNavigate()
-
-  const [formData, setFormData] = useState({
-    email: '',
-    password: '',
-  })
-
-  //getting state from reducer
-  const { userInfo, error, loading } = useSelector((state) => state.userLogin)
-
-  //If user is already logged in Do not show this page
-  // or redirect to complete - profile page if not already registered with social auth
-  useEffect(() => {
-    if (
-      userInfo &&
-      userInfo.alreadyRegistered !== undefined &&
-      !userInfo.alreadyRegistered
-    ) {
-      navigate('/complete-profile')
-    } else if (userInfo) {
-      navigate('/')
+    const dispatch = useDispatch()
+    const navigate = useNavigate()
+    //getting state from reducer
+    const { userInfo, error, loading } = useSelector((state) => state.userLogin)
+  
+    const [formData, setFormData] = useState({
+      email: '',
+      password: '',
+    })
+  
+    const handleChange = (e) => {
+      setFormData((f) => ({
+        ...f,
+        [e.target.name]: e.target.value,
+      }))
     }
-  }, [userInfo, navigate, loading])
-
-  // show error message
-  useEffect(() => {
-    if (error) {
-      swal('Error', error, 'error')
+  
+    //If user is already logged in Do not show this page
+    useEffect(() => {
+      if (userInfo) navigate('/')
+    }, [userInfo, navigate, loading])
+  
+    const handleSubmit = (e) => {
+      dispatch(userLogin(formData.email, formData.password))
     }
-  }, [error])
-
-  const handleChange = (e) => {
-    setFormData((f) => ({
-      ...f,
-      [e.target.name]: e.target.value,
-    }))
-  }
-
-  const handleSubmit = (e) => {
-    dispatch(userLogin(formData.email, formData.password))
-  }
-
-  return {
-    isMobile,
-    dispatch,
-    formData,
-    setFormData,
-    handleChange,
-    handleSubmit,
-    loading,
-    error,
-  }
+  
+    return{
+        handleSubmit, handleChange, loading, error , userInfo, formData
+    }
 }
