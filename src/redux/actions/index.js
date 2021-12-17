2d09f106ba62bb9705e83742d758e5b81a460203
souptik4572 <souptiksarkar4572@gmail.com>
2021-12-17 23:39:54 +0530

    setup redux for fetching newsfeed

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,3 +1,9 @@
 // All actions for sponsors
 export { fetchSponsorRequest, fetchSponsorSuccess, fetchSponsorError } from './sponsors'
 export * from './authActions.js'
+
+export {
+  fetchNewsfeedRequest,
+  fetchNewsfeedSuccess,
+  fetchNewsfeedError,
+} from './newsfeed'
