fcbbe1f94b6e1ae9bef2918d1c25d6a4991a76af
AmitrajitDas <amitrajitdas31@gmail.com>
2021-12-17 21:10:51 +0530

    fix: animation bug

--- src/pages/Home/home.js
+++ src/pages/Home/home.js
@@ -7,6 +7,7 @@ export const HomeLogic = () => {
 }
 
 export const moreFunction = () => {
+  document.getElementById('moreoptions').classList.remove('fade-out')
   document.getElementById('mainpage').classList.add('fade-out')
   setTimeout(() => {
     document.getElementById('mainpage').style.display = 'none'
@@ -15,6 +16,7 @@ export const moreFunction = () => {
 }
 export const backoptions = () => {
   document.getElementById('moreoptions').classList.add('fade-out')
+  document.getElementById('mainpage').classList.remove('fade-out')
   setTimeout(() => {
     document.getElementById('moreoptions').style.display = 'none'
     document.getElementById('mainpage').style.display = 'block'
