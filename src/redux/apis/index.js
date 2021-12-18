41e1c711cde8d869f35ce3a94661b09d74ce1451
souptik4572 <souptiksarkar4572@gmail.com>
2021-12-18 09:01:31 +0530

    set up API for setting new password

--- src/redux/apis/index.js
+++ src/redux/apis/index.js
@@ -1,3 +1,5 @@
 // All API's for sponsors
 export { getAllSponsors, getMainSponsors } from './sponsors'
 export { loginUser, registerUser, requestPasswordReset } from './auth.js'
+// All API's for Participant Management
+export { completeProfile, setNewPassword } from './authManagement'
