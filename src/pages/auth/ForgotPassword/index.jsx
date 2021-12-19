25e050409ac1fccc602654843c2bc62358e596ac
Somsubhra Das <das.somsubhra1@gmail.com>
2021-12-19 17:38:31 +0530

    patch: FP UI improvement

--- src/pages/auth/ForgotPassword/index.jsx
+++ src/pages/auth/ForgotPassword/index.jsx
@@ -1,33 +1,52 @@
-import { Container, Grid } from '@mui/material'
+import { Container, Grid, Typography, Button } from '@mui/material'
+import { useStyles } from '../auth.style.js'
+
 import Input from '../../../components/input'
 import { ForgotPasswordLogic } from './forgotPassword'
 
-const ForgetPassword = (props) => {
+const ForgotPassword = (props) => {
+  const classes = useStyles()
   const { handleChange, formData, loading, handleFormSubmit } = ForgotPasswordLogic()
 
   return (
-    <Container maxWidth="xl">
-      <Grid container spacing={10}>
-        <Grid item lg={12} md={12}>
-          <Input
-            width="100%"
-            label="Your Email"
-            type="email"
-            placeholder="John Doe"
-            name="email"
-            value={formData.email}
-            onChange={(e) => handleChange(e)}
-          />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <button onClick={handleFormSubmit}>Submit</button>
+    <form
+      onSubmit={(e) => {
+        e.preventDefault()
+        handleFormSubmit(e)
+      }}
+    >
+      <Container maxWidth="xl">
+        <Grid container spacing={10}>
+          <Grid item lg={12} md={12}>
+            <Input
+              width="100%"
+              label="Your Email"
+              type="email"
+              placeholder="John Doe"
+              name="email"
+              value={formData.email}
+              onChange={(e) => handleChange(e)}
+            />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <Typography align="center">
+              <Button
+                type="submit"
+                variant="contained"
+                color="secondary"
+                className={classes.submitButton}
+              >
+                Submit
+              </Button>
+            </Typography>
+          </Grid>
         </Grid>
-      </Grid>
-    </Container>
+      </Container>
+    </form>
   )
 }
 
-export default ForgetPassword
+export default ForgotPassword
