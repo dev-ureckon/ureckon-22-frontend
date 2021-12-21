3ccf43b84db04386f3ace3753094bb520ff988a8
Somsubhra Das <das.somsubhra1@gmail.com>
2021-12-22 00:48:26 +0530

    fix: major

--- src/pages/auth/Login/login.js
+++ src/pages/auth/Login/login.js
@@ -24,16 +24,7 @@ export const LoginLogic = () => {
 
   //If user is already logged in Do not show this page
   useEffect(() => {
-    if (userInfo) {
-      if (userInfo.alreadyRegistered !== undefined && !userInfo.alreadyRegistered) {
-        navigate('/complete-profile')
-      } else if (
-        (userInfo.alreadyRegistered !== undefined && userInfo.alreadyRegistered) ||
-        userInfo.alreadyRegistered === undefined
-      ) {
-        navigate('/')
-      }
-    }
+    if (userInfo) navigate('/')
   }, [userInfo, navigate, loading])
 
   const handleSubmit = (e) => {
