7ec0f25bc501aef8ff80b5aac2e7fe0dd8583494
souptik4572 <souptiksarkar4572@gmail.com>
2022-01-02 18:45:42 +0530

    add: toast display for user profile fetch

--- src/redux/apis/profile.js
+++ src/redux/apis/profile.js
@@ -3,13 +3,13 @@ import {
   fetchUserProfileRequest,
   fetchUserProfileSuccess,
   fetchUserProfileError,
+  showToastTimer,
 } from '../actions'
 
 export const getUserProfile = () => async (dispatch, getState) => {
   dispatch(fetchUserProfileRequest())
   const currentState = getState()
   const { accessToken } = currentState.userLogin.userInfo
-  console.log(accessToken)
   try {
     const response = await axios.get('/participant/management/profile', {
       headers: {
@@ -19,6 +19,7 @@ export const getUserProfile = () => async (dispatch, getState) => {
     const { user, authProvider, registeredEvents } = response.data
     dispatch(fetchUserProfileSuccess(user, authProvider, registeredEvents))
   } catch (error) {
-    dispatch(fetchUserProfileError(error.response.data.error))
+    dispatch(showToastTimer('Error while fetching user profile, try again!', 'error'))
+    dispatch(fetchUserProfileError(error))
   }
 }
