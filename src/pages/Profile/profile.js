36362dd403058c282692061e1fdbecb148c46ecb
Shubhamdutta2000 <Shubhamdutta2000@users.noreply.github.com>
2022-01-14 16:26:35 +0000

    (CI) Apply formatting changes

--- src/pages/Profile/profile.js
+++ src/pages/Profile/profile.js
@@ -8,59 +8,59 @@ import { updateUserProfile } from '../../redux/apis'
 // import swal from 'sweetalert'
 
 export const ProfileLogic = () => {
-    const dispatch = useDispatch()
-    const navigate = useNavigate()
-    const [formData, setFormData] = useState({
-        name: '',
-        email: '',
-        number: '',
-        password: '',
-        collegeName: '',
-        gender: '',
-    })
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    number: '',
+    password: '',
+    collegeName: '',
+    gender: '',
+  })
 
-    //getting state from reducer
-    const { userInfo, error, loading } = useSelector((state) => state.userRegister)
+  //getting state from reducer
+  const { userInfo, error, loading } = useSelector((state) => state.userRegister)
 
-    // If user is already logged in Do not show this page
-    // or redirect to complete - profile page if not already registered with social auth
-    useEffect(() => {
-        if (userInfo) {
-            navigate('/profile')
-        }
-    }, [userInfo, navigate, loading])
-
-    // show error message
-    useEffect(() => {
-        if (error) {
-            // swal('Error', error, 'error')
-            dispatch(showToastTimer(error, 'error'))
-        }
-    }, [error, dispatch])
-
-    const handleChange = (e) => {
-        setFormData((f) => ({
-            ...f,
-            [e.target.name]: e.target.value,
-        }))
+  // If user is already logged in Do not show this page
+  // or redirect to complete - profile page if not already registered with social auth
+  useEffect(() => {
+    if (userInfo) {
+      navigate('/profile')
     }
+  }, [userInfo, navigate, loading])
 
-    const handleSubmit = (e) => {
-        dispatch(
-            updateUserProfile({
-                name: formData.name,
-                college: formData.collegeName,
-                phone: formData.number,
-                gender: formData.gender
-            })
-        )
+  // show error message
+  useEffect(() => {
+    if (error) {
+      // swal('Error', error, 'error')
+      dispatch(showToastTimer(error, 'error'))
     }
+  }, [error, dispatch])
 
-    return {
-        handleChange,
-        formData,
-        error,
-        loading,
-        handleSubmit,
-    }
+  const handleChange = (e) => {
+    setFormData((f) => ({
+      ...f,
+      [e.target.name]: e.target.value,
+    }))
+  }
+
+  const handleSubmit = (e) => {
+    dispatch(
+      updateUserProfile({
+        name: formData.name,
+        college: formData.collegeName,
+        phone: formData.number,
+        gender: formData.gender,
+      })
+    )
+  }
+
+  return {
+    handleChange,
+    formData,
+    error,
+    loading,
+    handleSubmit,
+  }
 }
