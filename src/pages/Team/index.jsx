2874f93fcf95bb2a591a5c6157ea0d6ba812460f
Prasundas99 <prasundas115@gmail.com>
2022-01-25 16:56:04 +0530

    feat: themming config

---
+++ src/pages/Team/index.jsx
@@ -0,0 +1,18 @@
+import { Container, Grid, Typography } from '@mui/material'
+import React from 'react';
+
+export default function Team() {
+  return (
+      <Container>
+           <Typography variant='h1' color="primary">Faculty Coordinators</Typography>
+          <Typography variant='h2' color="primary">Faculty Coordinators</Typography>
+          <Typography variant='h3' color="primary">Faculty Coordinators</Typography>
+
+            <Grid container>
+                <Grid item md={3} xs={12} lg={3}>
+
+                </Grid>
+            </Grid>
+      </Container>
+  );
+}
