8f35301bb21de07bbefc0115f4c3359e998d3c22
Prasun Das <58937669+Prasundas99@users.noreply.github.com>
2022-01-25 17:21:27 +0530

    Update theme.js

--- src/theme.js
+++ src/theme.js
@@ -75,7 +75,7 @@ const theme = createTheme({
       purple: '#2A3B97',
     },
     error: {
-      main: 'linear-gradient(to right, #DD5E27, #E53D26)',
+      main: ' #dd5e27',
     },
     warning: { main: '#F49320' }, //yellow
     success: { main: '#0DB3B3' }, //green
