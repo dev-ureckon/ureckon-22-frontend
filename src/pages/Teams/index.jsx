2be88d2e6e63d7838c4edacdf482c4c130d73f0b
Prasundas99 <prasundas115@gmail.com>
2022-01-25 22:00:04 +0530

    reduce code smells

--- src/pages/Teams/index.jsx
+++ src/pages/Teams/index.jsx
@@ -1,4 +1,4 @@
-import { Container, Grid, Typography, Card, CardActions, Paper } from '@mui/material'
+import { Container, Grid, Typography, Card, Paper } from '@mui/material'
 import FacebookIcon from '@mui/icons-material/Facebook'
 import LinkedInIcon from '@mui/icons-material/LinkedIn'
 import TwitterIcon from '@mui/icons-material/Twitter'
@@ -22,71 +22,66 @@ export default function Team() {
               <Grid container>
                 {teamData &&
                   teamData.team &&
-                  item.teamList.map((teamData, index) => {
+                  item.teamList.map((teamMemberData, indexKey) => {
                     return (
-                      <Grid item md={3} xs={12} lg={3} key={index}>
+                      <Grid item md={3} xs={12} lg={3} key={indexKey}>
                         <div style={{ padding: '1rem' }}>
                           <Card>
                             <Paper
                               style={{
-                                background: `linear-gradient(#00000000, #000000a3),url(${teamData.picture})`,
+                                background: `linear-gradient(#00000000, #000000a3),url(${teamMemberData.picture})`,
                                 backgroundSize: 'cover',
                                 backgroundPosition: 'center',
                               }}
                               className={classes.teamMember_image}
                             >
                               <div className={classes.teamMember_name}>
-                                <Typography variant="h5">{teamData.name}</Typography>
+                                <Typography variant="h5">
+                                  {teamMemberData.name}
+                                </Typography>
                                 <Typography variant="subtitle2">
-                                  {teamData.title}
+                                  {teamMemberData.title}
                                 </Typography>
                               </div>
                             </Paper>
                             <Paper className={classes.teamMember_social}>
                               <Typography variant="subtitle2" align="center">
-                                Email: {teamData.email}
+                                Email: {teamMemberData.email}
                               </Typography>
-                              {console.log(teamData.social)}
                               <Typography variant="subtitle2" align="center">
-                                {teamData.social && teamData.social.facebook ? (
+                                {teamMemberData.social &&
+                                teamMemberData.social.facebook ? (
                                   <>
                                     <a
-                                      href={teamData.social.facebook}
+                                      href={teamMemberData.social.facebook}
                                       className={classes.anchor}
                                     >
-                                      <FacebookIcon
-                                        
-                                        className={classes.socialIcon}
-                                      />
+                                      <FacebookIcon className={classes.socialIcon} />
                                     </a>
                                   </>
                                 ) : (
                                   <></>
                                 )}
-                                {teamData.social && teamData.social.linkedIn ? (
+                                {teamMemberData.social &&
+                                teamMemberData.social.linkedIn ? (
                                   <>
                                     <a
-                                      href={teamData.social.linkedIn}
+                                      href={teamMemberData.social.linkedIn}
                                       className={classes.anchor}
                                     >
-                                      <LinkedInIcon
-                                        
-                                        className={classes.socialIcon}
-                                      />
+                                      <LinkedInIcon className={classes.socialIcon} />
                                     </a>
                                   </>
                                 ) : (
                                   <></>
                                 )}
-                                {teamData.social && teamData.social.twitter ? (
+                                {teamMemberData.social &&
+                                teamMemberData.social.twitter ? (
                                   <a
-                                    href={teamData.social.twitter}
+                                    href={teamMemberData.social.twitter}
                                     className={classes.anchor}
                                   >
-                                    <TwitterIcon
-                                      
-                                      className={classes.socialIcon}
-                                    />
+                                    <TwitterIcon className={classes.socialIcon} />
                                   </a>
                                 ) : (
                                   <></>
