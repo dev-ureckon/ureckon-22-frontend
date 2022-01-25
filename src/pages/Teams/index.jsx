4ba24059136d7bdeac69253c01fd61c96e5ab57a
Prasundas99 <prasundas115@gmail.com>
2022-01-25 21:20:56 +0530

    refactored code

--- src/pages/Teams/index.jsx
+++ src/pages/Teams/index.jsx
@@ -1,14 +1,12 @@
 import { Container, Grid, Typography, Card, CardActions, Paper } from '@mui/material'
-import React, { useEffect } from 'react'
-import { useSelector, useDispatch } from 'react-redux'
-import { getAllTeams } from '../../redux/apis'
+import FacebookIcon from '@mui/icons-material/Facebook'
+import LinkedInIcon from '@mui/icons-material/LinkedIn'
+import TwitterIcon from '@mui/icons-material/Twitter'
+import { TeamsLogic } from './Team.js'
+import { useStyles } from './teams.style.js'
 export default function Team() {
-  const dispatch = useDispatch()
-  const { teams: teamData } = useSelector((state) => state.teams)
-  useEffect(() => {
-    dispatch(getAllTeams())
-  }, [dispatch])
-
+  const { teamData } = TeamsLogic()
+  const classes = useStyles()
   console.log(teamData.team)
   return (
     <Container>
@@ -27,30 +25,49 @@ export default function Team() {
                   item.teamList.map((teamData, index) => {
                     return (
                       <Grid item md={3} xs={12} lg={3} key={index}>
-                        <div style={{padding: '1rem'}}>
+                        <div style={{ padding: '1rem' }}>
                           <Card>
                             <Paper
                               style={{
                                 background: `linear-gradient(#00000000, #000000a3),url(${teamData.picture})`,
-                                color: '#fff',
                                 backgroundSize: 'cover',
                                 backgroundPosition: 'center',
-                                height: '272px',
-                                position: 'relative',
                               }}
+                              className={classes.teamMember_image}
                             >
-                              <Typography
-                                style={{
-                                  padding: '0.6rem',
-                                  width: '100%',
-                                  position: 'absolute',
-                                  top: '85%',
-                                }}
-                              >
-                                {teamData.name}
+                              <div className={classes.teamMember_name}>
+                                <Typography variant="h5">{teamData.name}</Typography>
+                                <Typography variant="subtitle2">
+                                  {teamData.title}
+                                </Typography>
+                              </div>
+                            </Paper>
+                            <Paper className={classes.teamMember_social}>
+                              <Typography variant="subtitle2" align="center">
+                                Email: {teamData.email}
+                              </Typography>
+                              <Typography variant="subtitle2" align="center">
+                                {' '}
+                                <a href={teamData.social.facebook}>
+                                  <FacebookIcon
+                                    fontSize="small"
+                                    className={classes.socialIcon}
+                                  />
+                                </a>{' '}
+                                <a href={teamData.social.linkedIn}>
+                                  <LinkedInIcon
+                                    fontSize="small"
+                                    className={classes.socialIcon}
+                                  />
+                                </a>{' '}
+                                <a href={teamData.social.twitter}>
+                                  <TwitterIcon
+                                    fontSize="small"
+                                    className={classes.socialIcon}
+                                  />
+                                </a>{' '}
                               </Typography>
                             </Paper>
-                            <CardActions disableSpacing>Icons</CardActions>
                           </Card>
                         </div>
                       </Grid>
---
+++ src/pages/Teams/teams.style.js
@@ -0,0 +1,22 @@
+import { makeStyles } from '@mui/styles'
+export const useStyles = makeStyles((theme) => ({
+  teamMember_name: {
+    padding: '0.6rem',
+    width: '100%',
+    position: 'absolute',
+    top: '75%',
+  },
+  teamMember_image:{
+    color: '#fff',
+    height: '272px',
+    position: 'relative',
+  },
+  teamMember_social:{
+    color: '#fff',
+    background: theme.palette.secondary.purple,
+    margin: '0 auto'
+  },
+  socialIcon:{
+    margin: '0.1rem 0.5rem'
+  }
+}))
