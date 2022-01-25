95e4fca13acadd2ba3ab03e5bf17a2b61ce732d3
Prasundas99 <prasundas115@gmail.com>
2022-01-25 21:44:17 +0530

    fix: ui gliches

--- src/pages/Teams/teams.style.js
+++ src/pages/Teams/teams.style.js
@@ -10,15 +10,18 @@ export const useStyles = makeStyles((theme) => ({
     color: '#fff',
     height: '272px',
     position: 'relative',
+borderRadius: '0'
   },
   teamMember_social:{
     color: '#fff',
     background: theme.palette.secondary.purple,
-    margin: '0 auto'
+    margin: '0 auto',
+    borderRadius: '0'
   },
   socialIcon:{
     margin: '0.1rem 0.5rem',
     color: theme.palette.primary,
+    borderRadius: '0'
   },
   anchor:{
     textDecoration: 'none', color: '#fff'
