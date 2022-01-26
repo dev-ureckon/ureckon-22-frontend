2b94415ae0fa38780ebb394e88c19a90143cb22f
Prasundas99 <prasundas115@gmail.com>
2022-01-26 22:18:21 +0530

    accordin added

--- src/pages/Teams/index.jsx
+++ src/pages/Teams/index.jsx
@@ -4,6 +4,12 @@ import LinkedInIcon from '@mui/icons-material/LinkedIn'
 import TwitterIcon from '@mui/icons-material/Twitter'
 import { TeamsLogic } from './Team.js'
 import { useStyles } from './teams.style.js'
+
+import Accordion from '@mui/material/Accordion'
+import AccordionSummary from '@mui/material/AccordionSummary'
+import AccordionDetails from '@mui/material/AccordionDetails'
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore'
+
 export default function Team() {
   const { teamData } = TeamsLogic()
   const classes = useStyles()
@@ -13,92 +19,103 @@ export default function Team() {
       {teamData &&
         teamData.team &&
         teamData.team.map((item, index) => {
+          let expand = false;
+          (index === 0 || 1) ? expand = true : expand= false 
           return (
             <div key={index}>
-              <Typography variant="h2" color="primary">
-                {item.categoryName}
-              </Typography>
-
-              <Grid container>
-                {teamData &&
-                  teamData.team &&
-                  item.teamList.map((teamMemberData, indexKey) => {
-                    return (
-                      <Grid item md={3} xs={12} lg={3} key={indexKey}>
-                        <div style={{ padding: '1rem' }}>
-                          <Card>
-                            <Paper
-                              style={{
-                                background: `linear-gradient(#00000000, #000000a3),url(${teamMemberData.picture})`,
-                                backgroundSize: 'cover',
-                                backgroundPosition: 'center',
-                              }}
-                              className={classes.teamMember_image}
-                            >
-                              <div className={classes.teamMember_name}>
-                                <Typography variant="h5">
-                                  {teamMemberData.name}
-                                </Typography>
-                                <Typography variant="subtitle2">
-                                  {teamMemberData.title}
-                                </Typography>
-                              </div>
-                            </Paper>
-                            <Paper className={classes.teamMember_social}>
-                              <Typography variant="subtitle2" align="center">
-                                <a href={teamMemberData.email}>
-                                  Email: {teamMemberData.email}
-                                </a>
-                              </Typography>
-                              <Typography variant="subtitle2" align="center">
-                                {teamMemberData.social &&
-                                teamMemberData.social.facebook ? (
-                                  <>
-                                    <a
-                                      target="_blank"
-                                      href={teamMemberData.social.facebook}
-                                      className={classes.anchor}
-                                    >
-                                      <FacebookIcon className={classes.socialIcon} />
+              <Accordion style={{ background: '#fff0' }} defaultExpanded={expand}>
+                <AccordionSummary
+                  expandIcon={<ExpandMoreIcon style={{ color: '#fff' }} />}
+                  aria-controls="panel1a-content"
+                  id={index}
+                >
+                  <Typography variant="h2" color="primary">
+                    {item.categoryName}
+                  </Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                  <Grid container>
+                    {teamData &&
+                      teamData.team &&
+                      item.teamList.map((teamMemberData, indexKey) => {
+                        return (
+                          <Grid item md={3} xs={12} lg={3} key={indexKey}>
+                            <div style={{ padding: '1rem' }}>
+                              <Card>
+                                <Paper
+                                  style={{
+                                    background: `linear-gradient(#00000000, #000000a3),url(${teamMemberData.picture})`,
+                                    backgroundSize: 'cover',
+                                    backgroundPosition: 'center',
+                                  }}
+                                  className={classes.teamMember_image}
+                                >
+                                  <div className={classes.teamMember_name}>
+                                    <Typography variant="h5">
+                                      {teamMemberData.name}
+                                    </Typography>
+                                    <Typography variant="subtitle2">
+                                      {teamMemberData.title}
+                                    </Typography>
+                                  </div>
+                                </Paper>
+                                <Paper className={classes.teamMember_social}>
+                                  <Typography variant="subtitle2" align="center">
+                                    <a href={teamMemberData.email} className={classes.anchor}>
+                                      Email: {teamMemberData.email}
                                     </a>
-                                  </>
-                                ) : (
-                                  <></>
-                                )}
-                                {teamMemberData.social &&
-                                teamMemberData.social.linkedIn ? (
-                                  <>
-                                    <a
-                                      target="_blank"
-                                      href={teamMemberData.social.linkedIn}
-                                      className={classes.anchor}
-                                    >
-                                      <LinkedInIcon className={classes.socialIcon} />
-                                    </a>
-                                  </>
-                                ) : (
-                                  <></>
-                                )}
-                                {teamMemberData.social &&
-                                teamMemberData.social.twitter ? (
-                                  <a
-                                    target="_blank"
-                                    href={teamMemberData.social.twitter}
-                                    className={classes.anchor}
-                                  >
-                                    <TwitterIcon className={classes.socialIcon} />
-                                  </a>
-                                ) : (
-                                  <></>
-                                )}
-                              </Typography>
-                            </Paper>
-                          </Card>
-                        </div>
-                      </Grid>
-                    )
-                  })}
-              </Grid>
+                                  </Typography>
+                                  <Typography variant="subtitle2" align="center">
+                                    {teamMemberData.social &&
+                                    teamMemberData.social.facebook ? (
+                                      <>
+                                        <a
+                                          target="_blank"
+                                          href={teamMemberData.social.facebook}
+                                          className={classes.anchor}
+                                        >
+                                          <FacebookIcon className={classes.socialIcon} />
+                                        </a>
+                                      </>
+                                    ) : (
+                                      <></>
+                                    )}
+                                    {teamMemberData.social &&
+                                    teamMemberData.social.linkedIn ? (
+                                      <>
+                                        <a
+                                          target="_blank"
+                                          href={teamMemberData.social.linkedIn}
+                                          className={classes.anchor}
+                                        >
+                                          <LinkedInIcon className={classes.socialIcon} />
+                                        </a>
+                                      </>
+                                    ) : (
+                                      <></>
+                                    )}
+                                    {teamMemberData.social &&
+                                    teamMemberData.social.twitter ? (
+                                      <a
+                                        target="_blank"
+                                        href={teamMemberData.social.twitter}
+                                        className={classes.anchor}
+                                      >
+                                        <TwitterIcon className={classes.socialIcon} />
+                                      </a>
+                                    ) : (
+                                      <></>
+                                    )}
+                                  </Typography>
+                                </Paper>
+                              </Card>
+                            </div>
+                          </Grid>
+                        )
+                      })}
+                  </Grid>
+                </AccordionDetails>
+              </Accordion>
             </div>
           )
         })}
