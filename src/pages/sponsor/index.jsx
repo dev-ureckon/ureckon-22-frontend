638c1da1a123a9b58b1f4c211b9be0e3eb87f33a
Bihan001 <bhchakraborty001@gmail.com>
2022-01-27 01:46:07 +0530

    added: footer

--- src/pages/sponsor/index.jsx
+++ src/pages/sponsor/index.jsx
@@ -1,51 +1,41 @@
 import React from 'react'
 import './sponsor.css'
-import { Container, Grid } from '@mui/material'
+import { Container } from '@mui/material'
 
-function Sponsor(){
-    return(<Container>
-        <>
+const demoImg =
+  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQxkHLKWWnxn9Jna1EITHAVnucqW-hVIX2-fIihfn4WlwTYnTeMtOea31guel_EZu6j9tI&usqp=CAU'
+
+function Sponsor() {
+  return (
+    <Container>
+      <>
         <div className="one">
-        <h2>Event Partners</h2>
+          <h2>Event Partners</h2>
         </div>
         <div className="pic">
-            <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQxkHLKWWnxn9Jna1EITHAVnucqW-hVIX2-fIihfn4WlwTYnTeMtOea31guel_EZu6j9tI&usqp=CAU' />
-
-            <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQxkHLKWWnxn9Jna1EITHAVnucqW-hVIX2-fIihfn4WlwTYnTeMtOea31guel_EZu6j9tI&usqp=CAU' />
-            <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQxkHLKWWnxn9Jna1EITHAVnucqW-hVIX2-fIihfn4WlwTYnTeMtOea31guel_EZu6j9tI&usqp=CAU' />
-            <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQxkHLKWWnxn9Jna1EITHAVnucqW-hVIX2-fIihfn4WlwTYnTeMtOea31guel_EZu6j9tI&usqp=CAU' />
-            <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQxkHLKWWnxn9Jna1EITHAVnucqW-hVIX2-fIihfn4WlwTYnTeMtOea31guel_EZu6j9tI&usqp=CAU' />
-            <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQxkHLKWWnxn9Jna1EITHAVnucqW-hVIX2-fIihfn4WlwTYnTeMtOea31guel_EZu6j9tI&usqp=CAU' />
-
+          {new Array(6).fill(0).map((_) => (
+            <img src={demoImg} alt="sponsor" />
+          ))}
         </div>
         <div className="one">
-        <h2>Gaming Partners</h2>
+          <h2>Gaming Partners</h2>
         </div>
         <div className="pic">
-        <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQxkHLKWWnxn9Jna1EITHAVnucqW-hVIX2-fIihfn4WlwTYnTeMtOea31guel_EZu6j9tI&usqp=CAU' />
-        <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQxkHLKWWnxn9Jna1EITHAVnucqW-hVIX2-fIihfn4WlwTYnTeMtOea31guel_EZu6j9tI&usqp=CAU' />
-        <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQxkHLKWWnxn9Jna1EITHAVnucqW-hVIX2-fIihfn4WlwTYnTeMtOea31guel_EZu6j9tI&usqp=CAU' />
-        <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQxkHLKWWnxn9Jna1EITHAVnucqW-hVIX2-fIihfn4WlwTYnTeMtOea31guel_EZu6j9tI&usqp=CAU' />
-        <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQxkHLKWWnxn9Jna1EITHAVnucqW-hVIX2-fIihfn4WlwTYnTeMtOea31guel_EZu6j9tI&usqp=CAU' />
-        <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQxkHLKWWnxn9Jna1EITHAVnucqW-hVIX2-fIihfn4WlwTYnTeMtOea31guel_EZu6j9tI&usqp=CAU' />
-
+          {new Array(6).fill(0).map((_) => (
+            <img src={demoImg} alt="sponsor" />
+          ))}
         </div>
         <div className="one">
-        <h2>Food Partners</h2>
+          <h2>Food Partners</h2>
         </div>
         <div className="pic">
-        <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQxkHLKWWnxn9Jna1EITHAVnucqW-hVIX2-fIihfn4WlwTYnTeMtOea31guel_EZu6j9tI&usqp=CAU' />
-        <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQxkHLKWWnxn9Jna1EITHAVnucqW-hVIX2-fIihfn4WlwTYnTeMtOea31guel_EZu6j9tI&usqp=CAU' />
-        <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQxkHLKWWnxn9Jna1EITHAVnucqW-hVIX2-fIihfn4WlwTYnTeMtOea31guel_EZu6j9tI&usqp=CAU' />
-        <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQxkHLKWWnxn9Jna1EITHAVnucqW-hVIX2-fIihfn4WlwTYnTeMtOea31guel_EZu6j9tI&usqp=CAU' />
-        <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQxkHLKWWnxn9Jna1EITHAVnucqW-hVIX2-fIihfn4WlwTYnTeMtOea31guel_EZu6j9tI&usqp=CAU' />
-        <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQxkHLKWWnxn9Jna1EITHAVnucqW-hVIX2-fIihfn4WlwTYnTeMtOea31guel_EZu6j9tI&usqp=CAU' />
-
+          {new Array(6).fill(0).map((_) => (
+            <img src={demoImg} alt="sponsor" />
+          ))}
         </div>
-        
-        </>
-    </Container>)
+      </>
+    </Container>
+  )
 }
-
 
 export default Sponsor
