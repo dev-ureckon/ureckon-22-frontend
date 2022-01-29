502742798be3ca6301ae7e40cab54906ecc95723
Shubhamdutta2000 <dondutta2000@gmail.com>
2022-01-30 00:20:27 +0530

    [FIX] resolve some warning issue part 2

--- src/components/passwordModal/index.jsx
+++ src/components/passwordModal/index.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from 'react'
+import { useState, useRef } from 'react'
 import { useDispatch } from 'react-redux'
 import { Button } from '@mui/material'
 import { updateUserPassword } from '../../redux/apis/profile'
