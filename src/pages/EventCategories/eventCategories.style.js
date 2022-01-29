709d9f551606b4599da0e3b2a2f441b66c24f894
Shubhamdutta2000 <Shubhamdutta2000@users.noreply.github.com>
2022-01-29 11:33:03 +0000

    (CI) Apply formatting changes

--- src/pages/EventCategories/eventCategories.style.js
+++ src/pages/EventCategories/eventCategories.style.js
@@ -1,21 +1,21 @@
 import { makeStyles } from '@mui/styles'
 export const useStyles = makeStyles((theme) => ({
-    categoryPaper: {
-        /* Auto layout */
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'center',
-        padding: '36px',
-        position: 'static',
-        height: '252px',
-        left: '0px',
-        top: '0px',
-        background: 'rgba(255, 255, 255, 0.08)',
-        border: '3px solid #0DB3B3',
-        borderRadius: '5px',
-        flex: 'none',
-        order: '0',
-        flexGrow: '0',
-    }
+  categoryPaper: {
+    /* Auto layout */
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: '36px',
+    position: 'static',
+    height: '252px',
+    left: '0px',
+    top: '0px',
+    background: 'rgba(255, 255, 255, 0.08)',
+    border: '3px solid #0DB3B3',
+    borderRadius: '5px',
+    flex: 'none',
+    order: '0',
+    flexGrow: '0',
+  },
 }))
--- src/pages/EventCategories/index.jsx
+++ src/pages/EventCategories/index.jsx
@@ -1,24 +1,26 @@
-import { Container, Grid, Paper, Typography } from '@mui/material';
-import React from 'react';
-import { useStyles } from './eventCategories.style';
+import { Container, Grid, Paper, Typography } from '@mui/material'
+import React from 'react'
+import { useStyles } from './eventCategories.style'
 
-import eventCategorySample from "../../assets/SVGs/eventCategorySample.svg";
+import eventCategorySample from '../../assets/SVGs/eventCategorySample.svg'
 
 const EventCategories = () => {
-    const classes = useStyles()
-    return <Container>
-        <Typography variant='h2'>Event Category</Typography>
-        <Grid container>
-            <Grid item md={3} xs={12}>
-                <Paper className={classes.categoryPaper} >
-                    <img src={eventCategorySample} alt='event Category' />
-                    <Typography textAlign={'center'} variant='h4' style={{ color: '#fff' }}>
-                        Robotics Events
-                    </Typography>
-                </Paper>
-            </Grid>
+  const classes = useStyles()
+  return (
+    <Container>
+      <Typography variant="h2">Event Category</Typography>
+      <Grid container>
+        <Grid item md={3} xs={12}>
+          <Paper className={classes.categoryPaper}>
+            <img src={eventCategorySample} alt="event Category" />
+            <Typography textAlign={'center'} variant="h4" style={{ color: '#fff' }}>
+              Robotics Events
+            </Typography>
+          </Paper>
         </Grid>
-    </Container>;
-};
+      </Grid>
+    </Container>
+  )
+}
 
-export default EventCategories;
+export default EventCategories
