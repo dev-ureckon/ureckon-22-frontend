ba1ad883674d312730b5616ef1b6abd1594b31d5
Shubhamdutta2000 <Shubhamdutta2000@users.noreply.github.com>
2022-01-29 16:57:38 +0000

    (CI) Apply formatting changes

--- src/pages/EventCategories/index.jsx
+++ src/pages/EventCategories/index.jsx
@@ -14,20 +14,30 @@ const EventCategories = () => {
 
   return (
     <Container>
-      <Typography variant="h2" style={{ marginBottom: "20px" }}>Event Categories</Typography>
-      <Grid container spacing='40'>
-        {categories && categories.map((category) => (
-          <Grid item md={3} xs={12}>
-            <Link style={{ textDecoration: 'none' }} to={`/event/${category.categoryName}`}>
-              <Paper className={classes.categoryPaper}>
-                <img src={category.icon} style={{ width: '100px', marginBottom: "15px" }} alt="event Category" />
-                <Typography textAlign={'center'} variant="h4" style={{ color: '#fff' }}>
-                  {`${category.categoryName} Event`}
-                </Typography>
-              </Paper>
-            </Link>
-          </Grid>
-        ))}
+      <Typography variant="h2" style={{ marginBottom: '20px' }}>
+        Event Categories
+      </Typography>
+      <Grid container spacing="40">
+        {categories &&
+          categories.map((category) => (
+            <Grid item md={3} xs={12}>
+              <Link
+                style={{ textDecoration: 'none' }}
+                to={`/event/${category.categoryName}`}
+              >
+                <Paper className={classes.categoryPaper}>
+                  <img
+                    src={category.icon}
+                    style={{ width: '100px', marginBottom: '15px' }}
+                    alt="event Category"
+                  />
+                  <Typography textAlign={'center'} variant="h4" style={{ color: '#fff' }}>
+                    {`${category.categoryName} Event`}
+                  </Typography>
+                </Paper>
+              </Link>
+            </Grid>
+          ))}
       </Grid>
     </Container>
   )
