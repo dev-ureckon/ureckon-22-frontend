04299e3409430bcc07c8a2b47539b9ca0478f0cf
Bihan001 <bhchakraborty001@gmail.com>
2022-01-31 21:10:00 +0530

    integrated sponsors api update

--- src/pages/sponsor/index.jsx
+++ src/pages/sponsor/index.jsx
@@ -5,58 +5,31 @@ import { getAllSponsors } from '../../redux/apis/sponsors'
 import { useSelector } from 'react-redux'
 import { useDispatch } from 'react-redux'
 
-const demoImg =
-  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQxkHLKWWnxn9Jna1EITHAVnucqW-hVIX2-fIihfn4WlwTYnTeMtOea31guel_EZu6j9tI&usqp=CAU'
-
 function Sponsor() {
   const dispatch = useDispatch()
   const sponsors = useSelector((state) => state.sponsor.sponsors)
 
   useEffect(() => {
     dispatch(getAllSponsors())
-  }, [])
-
-  useEffect(() => {
-    console.log(sponsors)
-  }, [sponsors])
-
-  const eventSponsors = sponsors.find((s) => s.categoryName === 'Event')
-  const gamingSponsors = sponsors.find((s) => s.categoryName === 'Gaming')
-  const foodSponsors = sponsors.find((s) => s.categoryName === 'Food')
+  }, [dispatch])
 
   return (
     <Container style={{ minHeight: 'calc(100vh - 255px)' }}>
       <>
-        {!!eventSponsors && (
-          <div className="one">
-            <h2>Event Partners</h2>
-          </div>
-        )}
-        <div className="pic">
-          {eventSponsors?.sponsorList.map((s) => (
-            <img key={s.sponsorName} src={s.sponsorLogo} alt="sponsor" />
+        {sponsors
+          .sort((a, b) => a.order - b.order)
+          .map((sponsor) => (
+            <>
+              <div className="one">
+                <h2>{sponsor.categoryName}</h2>
+              </div>
+              <div className="pic">
+                {sponsor?.sponsorList.map((s) => (
+                  <img key={s.sponsorName} src={s.sponsorLogo} alt="sponsor" />
+                ))}
+              </div>
+            </>
           ))}
-        </div>
-        {!!gamingSponsors && (
-          <div className="one">
-            <h2>Gaming Partners</h2>
-          </div>
-        )}
-        <div className="pic">
-          {gamingSponsors?.sponsorList.map((s) => (
-            <img key={s.sponsorName} src={s.sponsorLogo} alt="sponsor" />
-          ))}
-        </div>
-        {foodSponsors && (
-          <div className="one">
-            <h2>Food Partners</h2>
-          </div>
-        )}
-        <div className="pic">
-          {foodSponsors?.sponsorList.map((s) => (
-            <img key={s.sponsorName} src={s.sponsorLogo} alt="sponsor" />
-          ))}
-        </div>
       </>
     </Container>
   )
