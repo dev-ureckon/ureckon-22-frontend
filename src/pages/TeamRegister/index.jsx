a1f6f81577a48db515efcd9b47ead70db58c7ecb
AmitrajitDas <amitrajitdas31@gmail.com>
2022-01-31 21:24:02 +0530

    fix: setup

--- src/pages/TeamRegister/index.jsx
+++ src/pages/TeamRegister/index.jsx
@@ -2,11 +2,11 @@ import './teamregister.css'
 import './../../components/TeamRegister/Form'
 
 const TeamRegister = () => {
-  return <div>
-    <>
-   <h2>Ayusha</h2>
-    </>
-  </div>
+  return (
+    <div>
+      <h2>Ayusha</h2>
+    </div>
+  )
 }
 
 export default TeamRegister
