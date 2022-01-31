6812eebd566a8d1d949028c43d91cc00f914d346
Ayusha Burman <ayushaburman2020@gmail.com>
2022-01-31 23:26:11 +0530

    Add: Form Handling

--- src/components/TeamRegister/Form/index.jsx
+++ src/components/TeamRegister/Form/index.jsx
@@ -6,16 +6,31 @@ import Dropdown from '../../../components/Dropdown'
 import SubmitButton from '../../../components/SubmitButton'
 
 const Form = () => {
-  return <div>
+  return (
+    <>
+    <div>
     <Container>
     <form style={{ paddingLeft: '26px' }}>
     <Grid item lg={6} md={6} sm={12} xs={12}>
        <Input
+       
         width="100%"
         label="Team Name"
         placeholder="Stephen Strange"
         name="name"
-        // value={formData.name}
+       // value={formData.name}
+        //onChange={(e) => handleChange(e)}
+       />
+       <div style={{height:50}}/>
+
+       
+       <Input
+        width="100%"
+        label="Team Leader"
+        
+        placeholder="Kaustav Royyy"
+        name="name"
+       // value={formData.name}
         //onChange={(e) => handleChange(e)}
        />
     </Grid>
@@ -23,7 +38,10 @@ const Form = () => {
 
       </form>
     </Container>
-  </div>
+    </div>
+    </>
+  )
+
 }
 
 export default Form
--- src/pages/TeamRegister/index.jsx
+++ src/pages/TeamRegister/index.jsx
@@ -1,11 +1,22 @@
 import './teamregister.css'
 import './../../components/TeamRegister/Form'
+import { Button, Container, Grid, LinearProgress, Typography } from '@mui/material'
+import Input from './../../components/input'
+import Form from './../../components/TeamRegister/Form'
 
 const TeamRegister = () => {
   return (
-    <div>
-      <h2>Ayusha</h2>
+    <>
+    
+    <div className="heading">
+      <Container>
+      <h2>Register Your Team</h2>
+      </Container>
+     
     </div>
+    <Form/>
+    
+    </>
   )
 }
 
