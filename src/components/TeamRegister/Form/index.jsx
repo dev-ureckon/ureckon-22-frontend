bb9e810e7b687552653f554f356bc75689ea81c1
Ayusha Burman <ayushaburman2020@gmail.com>
2022-01-31 21:16:03 +0530

    can't check

--- src/components/TeamRegister/Form/index.jsx
+++ src/components/TeamRegister/Form/index.jsx
@@ -1,7 +1,29 @@
 import React from 'react'
+import './form.css'
+import { Button, Container, Grid, LinearProgress, Typography } from '@mui/material'
+import Input from '../../../components/input'
+import Dropdown from '../../../components/Dropdown'
+import SubmitButton from '../../../components/SubmitButton'
 
 const Form = () => {
-  return <div></div>
+  return <div>
+    <Container>
+    <form style={{ paddingLeft: '26px' }}>
+    <Grid item lg={6} md={6} sm={12} xs={12}>
+       <Input
+        width="100%"
+        label="Team Name"
+        placeholder="Stephen Strange"
+        name="name"
+        // value={formData.name}
+        //onChange={(e) => handleChange(e)}
+       />
+    </Grid>
+    
+
+      </form>
+    </Container>
+  </div>
 }
 
 export default Form
--- src/pages/TeamRegister/index.jsx
+++ src/pages/TeamRegister/index.jsx
@@ -1,7 +1,12 @@
 import './teamregister.css'
+import './../../components/TeamRegister/Form'
 
 const TeamRegister = () => {
-  return <div></div>
+  return <div>
+    <>
+   <h2>Ayusha</h2>
+    </>
+  </div>
 }
 
 export default TeamRegister
