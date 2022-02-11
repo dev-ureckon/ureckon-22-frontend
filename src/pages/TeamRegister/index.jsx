d400ea8e74c496f023504a78f665e4a9fd2b9685
AmitrajitDas <amitrajitdas31@gmail.com>
2022-02-11 21:52:10 +0530

    feat: UI Done

--- src/pages/TeamRegister/index.jsx
+++ src/pages/TeamRegister/index.jsx
@@ -1,4 +1,5 @@
-import { Container } from '@mui/material'
+import { Container, Button } from '@mui/material'
+import SubmitButton from '../../components/SubmitButton/index.jsx'
 import Teammates from '../../components/TeamRegister/Teammates/index.jsx'
 import Input from './../../components/input'
 import './teamregister.css'
@@ -6,13 +7,20 @@ import Form from './../../components/TeamRegister/Form'
 
 const TeamRegister = () => {
   return (
-    <div className="teamReg-container">
-      <div className="teamReg-form-wrapper">
-        <div className="teamReg-header">Register Your Team</div>
-        <Form />
+    <>
+      <div className="teamReg-container">
+        <div className="teamReg-form-wrapper">
+          <div className="teamReg-header">Register Your Team</div>
+          <Form />
+        </div>
+        <Teammates />
       </div>
-      <Teammates />
-    </div>
+      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+        <Button>
+          <SubmitButton label={'Register'} />
+        </Button>
+      </div>
+    </>
   )
 }
 
