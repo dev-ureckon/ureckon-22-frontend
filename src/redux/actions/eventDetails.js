953b04d831a8252a964424feaa001514bb90fbd5
Shubhamdutta2000 <Shubhamdutta2000@users.noreply.github.com>
2022-03-10 11:34:32 +0000

    (CI) Apply formatting changes

--- src/redux/actions/eventDetails.js
+++ src/redux/actions/eventDetails.js
@@ -1,20 +1,20 @@
 import {
-    EVENT_DETAILS_REQUEST,
-    EVENT_DETAILS_SUCCESS,
-    EVENT_DETAILS_FAILURE,
+  EVENT_DETAILS_REQUEST,
+  EVENT_DETAILS_SUCCESS,
+  EVENT_DETAILS_FAILURE,
 } from '../constants'
 
 export const fetchEventDetailsRequest = () => ({
-    type: EVENT_DETAILS_REQUEST,
+  type: EVENT_DETAILS_REQUEST,
 })
 
 export const fetchEventDetailsSuccess = (event) => ({
-    type: EVENT_DETAILS_SUCCESS,
-    payload: event,
+  type: EVENT_DETAILS_SUCCESS,
+  payload: event,
 })
 
 export const fetchEventDetailsError = (error) => ({
-    // here error is basically the error message
-    type: EVENT_DETAILS_FAILURE,
-    payload: error,
+  // here error is basically the error message
+  type: EVENT_DETAILS_FAILURE,
+  payload: error,
 })
