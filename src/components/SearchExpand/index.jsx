7020016932f8108e1992684da9f1c757e5cb9b6d
swetakarar <kararsweta@gmail.com>
2022-03-13 12:41:24 +0530

    fix: search bar minor changes

--- src/components/SearchExpand/index.jsx
+++ src/components/SearchExpand/index.jsx
@@ -8,32 +8,48 @@ import {
   Container,
   Box,
   Typography,
+  FormControl,
+  Input,
+  InputAdornment,
 } from '@mui/material'
 import { makeStyles } from '@mui/styles'
 
 const useStyles = makeStyles({
   icon: {
     color: 'white',
-    height: '40px',
-    width: '40px',
+    height: '35px',
+    width: '35px',
   },
   searchText: {
     color: 'white',
     fontSize: '20px',
-    borderBottom: '2px solid white',
-    transition: 'all 0.2 linear',
+    // borderBottom: '2px solid white',
+    // transition: 'all 0.2 linear',
+    width: '30px',
   },
 })
 
 function SearchExpand() {
   const classes = useStyles()
   return (
-    <div>
-      <IconButton size="large" aria-label="search" color="inherit">
-        <SearchIcon className={classes.icon} />
-        <input type="text" className={classes.searchText} placeholder="Search here" />
-      </IconButton>
-    </div>
+    <>
+      <Box>
+        <FormControl variant="standard">
+          <Input
+            id="input-with-icon-adornment"
+            startAdornment={
+              <InputAdornment position="start" className={classes.searchText}>
+                <SearchIcon className={classes.icon} />
+              </InputAdornment>
+            }
+          />
+        </FormControl>
+        {/* <IconButton size="large" aria-label="search" color="inherit">
+          <SearchIcon className={classes.icon} />
+          <input type="text" className={classes.searchText} placeholder="Search here" />
+        </IconButton> */}
+      </Box>
+    </>
   )
 }
 
