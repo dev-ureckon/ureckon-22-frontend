7a276d73ae61ba6f19a4c135c2885bc458d2bda1
Shubhamdutta2000 <Shubhamdutta2000@users.noreply.github.com>
2022-03-16 13:46:30 +0000

    (CI) Apply formatting changes

--- src/pages/EventsSearch/EventsSearch.js
+++ src/pages/EventsSearch/EventsSearch.js
@@ -5,26 +5,26 @@ import { useDispatch, useSelector } from 'react-redux'
 import { getEventsSearch } from '../../redux/apis/eventList'
 
 export const EventSearchLogic = () => {
-    const dispatch = useDispatch()
-    const [searchedEventName, setSearchedEventName] = useState("")
-    const [eventsList, setEventsList] = useState([])
+  const dispatch = useDispatch()
+  const [searchedEventName, setSearchedEventName] = useState('')
+  const [eventsList, setEventsList] = useState([])
 
-    const { loading, events } = useSelector((state) => state.eventSearch)
+  const { loading, events } = useSelector((state) => state.eventSearch)
 
-    useEffect(() => {
-        if (searchedEventName) {
-            dispatch(getEventsSearch(searchedEventName))
-        }
-    }, [dispatch, searchedEventName])
-
-    useEffect(() => {
-        if (events && events.events != []) {
-            setEventsList(events.events)
-        }
-    }, [events])
-
-    return {
-        eventsList,
-        setSearchedEventName
+  useEffect(() => {
+    if (searchedEventName) {
+      dispatch(getEventsSearch(searchedEventName))
     }
+  }, [dispatch, searchedEventName])
+
+  useEffect(() => {
+    if (events && events.events != []) {
+      setEventsList(events.events)
+    }
+  }, [events])
+
+  return {
+    eventsList,
+    setSearchedEventName,
+  }
 }
